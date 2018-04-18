f8e347a382946af0a6ccf2cfdbc533609a403a8e
amrk7s <amruth.s@flipkart.com>
2018-04-18 13:36:30 +0530

    FDPIN-2965 Publish host IP instead of name to zookeeper during hive server starts

--- service/src/java/org/apache/hive/service/server/HiveServer2.java
+++ service/src/java/org/apache/hive/service/server/HiveServer2.java
@@ -477,7 +477,7 @@ public class HiveServer2 extends CompositeService {
     if ((thriftCLIService == null) || (thriftCLIService.getServerIPAddress() == null)) {
       throw new Exception("Unable to get the server address; it hasn't been initialized yet.");
     }
-    return thriftCLIService.getServerIPAddress().getHostName() + ":"
+    return thriftCLIService.getServerIPAddress().getHostAddress() + ":"
         + thriftCLIService.getPortNumber();
   }
 
